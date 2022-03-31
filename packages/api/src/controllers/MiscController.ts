a9ea6aac171712711cbfe0dc35bf70dec34e4027
OgbeniHMMD <olajide.a.hammed@gmail.com>
2022-03-31 22:16:22 +0100

    Added NotImplemented Controller

--- packages/api/src/controllers/MiscController.ts
+++ packages/api/src/controllers/MiscController.ts
@@ -1,5 +1,13 @@
 import { Response, Request } from "express";
 
+export function NotImplemented(req: Request, res: Response) {
+  const response = {
+    status: 501,
+    message: "Not Implemented",
+  };
+  return res.status(response.status).json(response);
+}
+
 export function NotFound(req: Request, res: Response) {
   const { method, url } = req;
   const response = {
