7958c6979d212c7a5cf7b9e330d4884f062bd5ab
OgbeniHMMD <olajide.a.hammed@gmail.com>
2022-04-24 05:47:59 +0100

    CreateUser logic

--- controllers/UsersController.ts
+++ controllers/UsersController.ts
@@ -1,3 +1,4 @@
+import { Users } from "./../models/index";
 import { Response, Request } from "express";
 
 export function GetUser(req: Request, res: Response) {
@@ -8,11 +9,33 @@ export function GetUser(req: Request, res: Response) {
   return res.status(response.status).json(response);
 }
 
-export function CreateUser(req: Request, res: Response) {
-  const response = {
+export async function CreateUser(req: Request, res: Response) {
+  let response: Res = {
     status: 501,
     message: "User not created",
   };
+
+  const { username, password, email } = req.body;
+  await Users.create({
+    username,
+    password,
+    email,
+  })
+    .then((res: any) => {
+      response = {
+        status: 200,
+        message: "Registration Successful",
+        data: res.dataValues,
+      };
+    })
+    .catch((e: any) => {
+      response = {
+        status: 500,
+        message: "An error occurred",
+        exception: e.errors.map((ex: any) => ex.message),
+      };
+    });
+
   return res.status(response.status).json(response);
 }
 
@@ -30,4 +53,11 @@ export function DeleteUser(req: Request, res: Response) {
     message: "User not deactivated",
   };
   return res.status(response.status).json(response);
+}
+
+interface Res {
+  status: number;
+  message: string;
+  exception?: any;
+  data?: Object;
 }
